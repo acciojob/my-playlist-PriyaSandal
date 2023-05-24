da65cfe9e219bc101c3eb4acade81e55203d0fe8
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-05-24 15:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+body{
+	background-color:#cccccc;
+}
+h1,h2{
+	text-align:center;
+}
+
+.player_one{
+	border:1px solid black;
+	border-radius:5px;
+	background-color:white;
+/* 	margin-bottom:10px; */
+}
+
+.player_one a{
+	text-decoration:none;
+	color:black;
+}
+
+.player_two{
+	border:1px solid black;	
+	padding:5px;
+	margin:0px 100px;
+	background-color:rgb(255,255,255);
+	border-radius:5px;
+}
+li{
+	list-style-type:none;
+}
